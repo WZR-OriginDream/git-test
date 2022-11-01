d39ffb7ec6a37596adf553721b7b18df278b9476
wen <wen@qq.com>
2022-11-01 11:24:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Origin Dream
+ * @Date: 2022/11/1 11:18
+ * @Decsription: JavaWeb Study
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
