322878237d04dc37259abcba2cd464551e10f87c
wen <wen@qq.com>
2022-11-01 12:40:10 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("master");
         System.out.println("hoyt-fix");
+        System.out.println("push");
     }
 }
